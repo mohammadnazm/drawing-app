ceed428ca1b91c92d23238960c7e1456d2f0cf7c
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-06 21:31:32 +0300

    Update Canvas.js

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -1,7 +1,11 @@
 import React from "react"
 
 const Canvas = () => {
-  return <canvas width={width} height={height} />
+  return <canvas width={width} height={height} style={canvasStyle} />
 }
 
 export default Canvas
+
+const canvasStyle = {
+    border: "1px solid black";
+}
