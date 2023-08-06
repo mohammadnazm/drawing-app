7c1987cb731e9882cd355a90409c65bc9da1fd9f
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-06 21:33:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css"
+import Canvas from "./Components/Canvas"
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Canvas width={700} height={500} />
+    </div>
+  )
 }
 
 export default App
