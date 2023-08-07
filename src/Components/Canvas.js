1d13f27f8807fbfafb00150060dca1ce5a8a5d27
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-07 12:01:20 +0300

    Update Canvas.js

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -4,17 +4,18 @@ import { useOnDraw } from "./Hooks"
 const Canvas = ({ width, height }) => {
   const setCanvasRef = useOnDraw(onDraw)
 
-  function onDraw(ctx, point) {
-    ctx.fillStyle = "#000000"
-    ctx.beginPath()
-    ctx.arc(point.x, point.y, 2, 0, 2 * Math.PI)
-    ctx.fill()
+  function onDraw(ctx, point, prevPoint) {
+    drawLine(prevPoint, point, ctx, "#000000", 5)
   }
 
   function drawLine(start, end, ctx, color, width) {
+    start = start ?? end
     ctx.beginPath()
     ctx.lineWidth = width
     ctx.strokeStyle = color
+    ctx.moveTo(start.x, start.y)
+    ctx.lineTo(end.x, end.y)
+    ctx.stroke()
   }
 
   return (
